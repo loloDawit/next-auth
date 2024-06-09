3decfe5c1d9271c71697966d71a95fba30dd3be7
lolodawit <dawitabera64@gmail.com>
2024-06-09 14:30:31 -0700

    feat: Add registration form component
    This commit adds a new component called `RegisterForm` to handle user registration. The component includes form fields for name, email, and password, along with validation and error handling. It also integrates with the `register` action to submit the registration data. This addition enhances the user registration functionality and improves the overall user experience.

---
+++ components/auth/register-form.tsx
@@ -0,0 +1,112 @@
+'use client';
+
+import { useState, useTransition } from 'react';
+import * as z from 'zod';
+import { CardWrapper } from './card-wrapper';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { RegisterSchema } from '@/schemas';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { FormError } from '@/components/form-error';
+import { FormSuccess } from '@/components/form-success';
+import { register } from '@/actions/register';
+
+export const RegisterForm = () => {
+  const [isPending, startTransition] = useTransition();
+  const [success, setSuccess] = useState<string | undefined>('');
+  const [error, setError] = useState<string | undefined>('');
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: { email: '', password: '', name: ''},
+  });
+
+  const onSubmit = (data: z.infer<typeof RegisterSchema>) => {
+    setError('');
+    setSuccess('');
+    startTransition(() => {
+      register(data).then((response) => {
+        setSuccess(response.success);
+        setError(response.error);
+      });
+    });
+  };
+
+  return (
+    <CardWrapper
+      headerLabel="Create an account"
+      backButtonHref="/auth/login"
+      backButtonLabel="Aready have an account?"
+      showSocial
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+          <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="your name here"
+                      type="name"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="your email here"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} disabled={isPending} placeholder="********" type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+          <Button type="submit" size="lg" className="w-full" disabled={isPending}>
+            Create an account
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+};
