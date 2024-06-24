268d5de4fc00e1b109195b81cdf4aa295b265ff1
lolodawit <dawitabera64@gmail.com>
2024-06-23 22:24:05 -0700

    feat: Add social login functionality
    This commit adds social login functionality to the application. The `Social` component in `components/auth/social.tsx` now includes buttons for Google and GitHub login. Clicking on these buttons triggers the `handleClick` function, which uses the `signIn` function from `next-auth/react` to initiate the login process. The callback URL is set to the default login redirect. This addition enhances the user authentication options and improves the overall user experience.

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,14 +1,22 @@
 'use client';
+
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
 import { Button } from '../ui/button';
+import { signIn } from 'next-auth/react';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 const Social = () => {
+  const handleClick = (provider: 'google' | 'github') => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
   return (
     <div className="flex w-full items-center gap-x-2">
-      <Button className="w-full" variant="outline" size="lg" onClick={() => {}}>
+      <Button className="w-full" variant="outline" size="lg" onClick={() => handleClick('google')}>
         <FcGoogle className="w-f h-5" />
       </Button>
-      <Button className="w-full" variant="outline" size="lg" onClick={() => {}}>
+      <Button className="w-full" variant="outline" size="lg" onClick={() => handleClick('github')}>
         <FaGithub className="w-f h-5" />
       </Button>
     </div>
