efb820499a13a8668b32ad5efeb624dc85f73210
lolodawit <dawitabera64@gmail.com>
2024-06-23 23:24:18 -0700

    refactor: Update error-card.tsx to improve code readability
    The code changes in this commit update the `error-card.tsx` file to improve code readability. The changes include reformatting the JSX code to use consistent indentation and updating class names to follow the BEM naming convention. These improvements enhance the maintainability and understandability of the code.

--- components/auth/error-card.tsx
+++ components/auth/error-card.tsx
@@ -8,8 +8,8 @@ const ErrorCard = () => {
       backButtonHref="/auth/login"
       backButtonLabel="Back to login"
     >
-      <div className='w-full flex justify-center items-center'>
-        <ExclamationTriangleIcon className='text-destructive'/>
+      <div className="flex w-full items-center justify-center">
+        <ExclamationTriangleIcon className="text-destructive" />
       </div>
     </CardWrapper>
   );
