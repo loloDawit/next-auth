eb6b486d7f2b83da38e6de6fd5b58986f3ac24a6
lolodawit <dawitabera64@gmail.com>
2024-06-27 21:24:19 -0700

    feat: Improve password complexity validation in login and registration forms and add ForgotPasswordSchema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,8 @@
 import * as z from 'zod';
 
+const passwordComplexityRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]).{8,}$/;
+
 export const LoginSchema = z.object({
   email: z.string().email({
     message: 'Please enter a valid email address',
@@ -8,6 +11,35 @@ export const LoginSchema = z.object({
     message: 'Password must be at least 8 characters',
   }),
 });
+
+export const ResetPasswordSchema = z.object({
+  email: z.string().email({
+    message: 'Please enter a valid email address',
+  }),
+});
+
+export const ForgotPasswordSchema = z
+  .object({
+    password: z.string().min(8, { message: 'Password must be at least 8 characters' }),
+    confirmPassword: z.string().min(8, { message: 'Password must be at least 8 characters' }),
+  })
+  .superRefine(({ password, confirmPassword }, context) => {
+    if (password !== confirmPassword) {
+      context.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ['confirmPassword'],
+        message: 'Passwords do not match',
+      });
+    }
+    if (!passwordComplexityRegex.test(password)) {
+      context.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ['password'],
+        message:
+          'Password must be 8+ characters, include upper, lower, number, and special characters',
+      });
+    }
+  });
 
 export const RegisterSchema = z
   .object({
@@ -20,9 +52,6 @@ export const RegisterSchema = z
     name: z.string().trim().min(2, { message: 'Name must be more than 1 character' }),
   })
   .superRefine(({ password }, checkPassComplexity) => {
-    const passwordComplexityRegex =
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]).{8,}$/;
-
     if (!passwordComplexityRegex.test(password)) {
       checkPassComplexity.addIssue({
         code: z.ZodIssueCode.custom,
