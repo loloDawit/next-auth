890fcd49b4c832052190fcbeddcf8ce80b1cf2cf
lolodawit <dawitabera64@gmail.com>
2024-06-27 21:26:06 -0700

    feat: Add password reset page
    This commit adds a new page component for the password reset feature. The `ResetPasswordPage` component renders a `ResendEmailForm` component, allowing users to reset their password by entering their email address and receiving a password reset link.

---
+++ app/auth/reset/page.tsx
@@ -0,0 +1,8 @@
+import ResendEmailForm from '@/components/auth/resend-email-form';
+import React from 'react';
+
+const ResetPasswordPage = () => {
+  return <ResendEmailForm />;
+};
+
+export default ResetPasswordPage;
