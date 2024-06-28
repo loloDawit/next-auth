cb88361349b8ada6db1b79b62e7231b1fcd4fa4a
lolodawit <dawitabera64@gmail.com>
2024-06-27 21:25:34 -0700

    feat: Add generatePasswordResetToken function for password reset
    This commit adds the generatePasswordResetToken function to the tokens.ts file. The function generates a password reset token for a given email address. If there is an existing token for the email, it is deleted before generating a new one. The token has an expiration time of 1 hour and is stored in the passwordResetToken table in the database.

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,6 +1,7 @@
 import { getVerificationTokenByEmail } from '@/data/verification-token';
 import { v4 as uuidv4 } from 'uuid';
 import { db } from '@/lib/db';
+import { getPasswordResetTokenByEmail } from '@/data/password-reset-token';
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
@@ -15,6 +16,27 @@ export const generateVerificationToken = async (email: string) => {
   }
   // genreate new token
   return await db.verificationToken.create({
+    data: {
+      email,
+      token,
+      expiresAt,
+    },
+  });
+};
+
+export const generatePasswordResetToken = async (email: string) => {
+  const token = uuidv4();
+  // token expiration is 1 hour
+  const expiresAt = new Date(Date.now() + 1000 * 60 * 60);
+
+  const hasExistingToken = await getPasswordResetTokenByEmail(email);
+  if (hasExistingToken) {
+    await db.passwordResetToken.delete({
+      where: { id: hasExistingToken.id },
+    });
+  }
+  // genreate new token
+  return await db.passwordResetToken.create({
     data: {
       email,
       token,
